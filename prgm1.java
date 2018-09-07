9b2f836eb6bf1126f5ac385cbfea4da6afbf71c4
nimamohan167 <43032891+nimamohan167@users.noreply.github.com>
2018-09-07 11:44:55 +0530

    Add files via upload

---
+++ prgm1.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+   double b,c,d,e,f;
+    a=10;
+    b=20.67;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println(c+" "+d+" "+e+" "+f+" ");
+  }
+}
