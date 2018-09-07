40565380a989c9ba3b895ca3a6908317bbf1e62b
nimamohan167 <43032891+nimamohan167@users.noreply.github.com>
2018-09-07 12:08:19 +0530

    Add files via upload

---
+++ pgm int.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,A,S,M;
+    a=2;
+    b=3;
+    A = a+b;
+    S = a-b;
+    M = a*b;
+    System.out.println(A+"  "+S+" "+M);
+  }
+}
