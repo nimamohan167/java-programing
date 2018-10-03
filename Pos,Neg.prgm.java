8647487a1d6d3cbf8464ff471762979f81ab85e0
nimamohan167 <43032891+nimamohan167@users.noreply.github.com>
2018-10-03 12:32:36 +0530

    Add files via upload

---
+++ Pos,Neg.prgm.java
@@ -0,0 +1,12 @@
+class Main{
+public static void main (String[]args) {
+   int number;
+   number=3;
+        if (number < 0)
+            System.out.println(number + " is a negative number.");
+        else if ( number > 0)
+            System.out.println(number + " is a positive }number.");
+        else
+            System.out.println(number + " is 0");
+}
+}
